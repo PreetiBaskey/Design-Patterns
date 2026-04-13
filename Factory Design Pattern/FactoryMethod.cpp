afcfe29a848185360f44eab346ef3faf1e4e83ae
React Native Developer <38905783+PreetiBaskey@users.noreply.github.com>
2026-04-13 16:25:27 +0530

    Create FactoryMethod.cpp

---
+++ Factory Design Pattern/FactoryMethod.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+
+using namespace std;
+
+class Burger {
+    public:
+        virtual void prepare() = 0;
+        ~Burger() {}
+};
+
+class BasicBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Preparing basic burger ...";
+        }
+};
+
+class StandardBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Preparing standard burger ...";
+        }
+};
+
+class PremiumBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Prepare premium burger ...";
+        }
+};
+
+class BasicWheatBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Preparing Wheat basic burger ...";
+        }
+};
+
+class StandardWheatBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Preparing Wheat standard burger ...";
+        }
+};
+
+class PremiumWheatBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Preparing Wheat premium burger ...";
+        }
+};
+
+
+class BurgerFactory {
+    public:
+        virtual Burger* createBurger(string &type) = 0;
+        ~BurgerFactory() {}
+};
+
+class NormalBurgerFactory : public BurgerFactory {
+    public:
+        Burger* createBurger(string &type) override {
+            if(type == "BASIC") {
+                return new BasicBurger();
+            }
+            else if(type == "STANDARD") {
+                return new StandardBurger();
+            }
+            else if(type == "PREMIUM") {
+                return new PremiumBurger();
+            }
+            else {
+                cout<<"Invalid burger ...";
+                return nullptr;
+            }
+        }
+};
+
+class WheatBurgerFactory : public BurgerFactory {
+    public:
+        Burger* createBurger(string &type) override {
+            if(type == "BASIC") {
+                return new BasicWheatBurger();
+            }
+            else if(type == "STANDARD") {
+                return new StandardWheatBurger();
+            }
+            else if(type == "PREMIUM") {
+                return new PremiumWheatBurger();
+            }
+            else {
+                cout<<"Invalid burger ...";
+                return nullptr;
+            }
+        }
+};
+
+
+int main() {
+    
+    string type = "STANDARD";
+    
+    BurgerFactory* burgerFactory = new NormalBurgerFactory();
+    
+    Burger* burger = burgerFactory->createBurger(type);
+    
+    burger->prepare();
+    
+    return 0;
+}
